b776a336aaf522d37fc2e3a5ab5f10c492778008
Codingwithriha <98806732+codingwithriha@users.noreply.github.com>
2025-06-23 11:43:23 +0500

    Initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+import { Component } from "react";
+import "./NavbarStyles.css";
+import { MenuItems } from "./MenuItems";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="NavbarItems">
+        <h1 className="navbar-logo">Trippy</h1>
+        <ul className="navbar-menu">
+          {MenuItems.map((item, index) => {
+            return (
+              <li>
+                <a className={item.cName} href={item.url}>
+                  <i className={item.icon}></i>
+                  {item.title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
