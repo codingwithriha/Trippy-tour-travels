43bbb3f76cb71fa7bdaeae702bc170a6ed5928fb
Codingwithriha <98806732+codingwithriha@users.noreply.github.com>
2025-06-24 17:45:32 +0000

    feat:hero-section-complete

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,4 +1,5 @@
 import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
 import ContactImg from "../assets/2.jpg";
 function Contact() {
   return (
--- src/routes/Service.js
+++ src/routes/Service.js
@@ -1,4 +1,5 @@
 import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
 import AboutImg from "../assets/night.jpg";
 function Service() {
   return (
