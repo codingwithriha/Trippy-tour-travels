72164de574835d903bc69b25a7175a5048e420d0
Codingwithriha <98806732+codingwithriha@users.noreply.github.com>
2025-06-24 07:23:31 +0000

    Inprgs:hero-section

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,19 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
+
 function Home() {
   return (
     <>
       <Navbar />
-      <Hero />
+      <Hero
+        cName="hero"
+        heroImg="https://unsplash.com/photos/man-and-woman-kissing-statue-near-city-buildings-during-daytime-BZ8IvPMuQ20"
+        title="Your Journey Your Story"
+        text="Choose Your Favourite Destination."
+        btnText="Travel Plan"
+        url="/"
+        btnClass="show"
+      />
     </>
   );
 }
